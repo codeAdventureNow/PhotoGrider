dfde80d4bf052408502602893e50884670ce5ee3
[David <[nmueller855@gmail.com]>
2023-04-26 15:26:19 -0700

    357

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,26 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from '../store';
+
 function UsersList() {
-  return 'Users List';
+  const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users; // {data: [], isLoading: false, erro: null}
+  });
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+
+  return <div>{data.length}</div>;
 }
 
 export default UsersList;
