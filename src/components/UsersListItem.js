905a4f3b8b44e9518427da4f4ad0bc201cf6fb5b
[David <[nmueller855@gmail.com]>
2023-04-27 11:30:02 -0700

    380

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -3,6 +3,7 @@ import Button from './Button';
 import { removeUser } from '../store';
 import { useThunk } from '../hooks/use-thunk';
 import ExpandablePanel from './ExpandablePanel';
+import AlbumsList from './AlbumsList';
 
 function UsersListItem({ user }) {
   const [doRemoverUser, isLoading, error] = useThunk(removeUser);
@@ -20,7 +21,11 @@ function UsersListItem({ user }) {
       {user.name}
     </>
   );
-  return <ExpandablePanel header={header}>Content!</ExpandablePanel>;
+  return (
+    <ExpandablePanel header={header}>
+      <AlbumsList user={user} />
+    </ExpandablePanel>
+  );
 }
 
 export default UsersListItem;
