261ae0aae2256377345edbe28f48d4e7e14c0c5c
[David <[nmueller855@gmail.com]>
2023-04-27 11:10:49 -0700

    378

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -2,6 +2,7 @@ import { GoTrashcan } from 'react-icons/go';
 import Button from './Button';
 import { removeUser } from '../store';
 import { useThunk } from '../hooks/use-thunk';
+import ExpandablePanel from './ExpandablePanel';
 
 function UsersListItem({ user }) {
   const [doRemoverUser, isLoading, error] = useThunk(removeUser);
@@ -9,19 +10,17 @@ function UsersListItem({ user }) {
   const handleClick = () => {
     doRemoverUser(user);
   };
-  return (
-    <div className='mb-2 border rounded'>
-      <div className='flex p-2 justify-between items-center cursor-pointer'>
-        <div className='flex flex-row items-center justify-between'>
-          <Button className='mr-3' loading={isLoading} onClick={handleClick}>
-            <GoTrashcan />
-          </Button>
-          {error && <div>Error deleting user.</div>}
-          {user.name}
-        </div>
-      </div>
-    </div>
+
+  const header = (
+    <>
+      <Button className='mr-3' loading={isLoading} onClick={handleClick}>
+        <GoTrashcan />
+      </Button>
+      {error && <div>Error deleting user.</div>}
+      {user.name}
+    </>
   );
+  return <ExpandablePanel header={header}>Content!</ExpandablePanel>;
 }
 
 export default UsersListItem;
