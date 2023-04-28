a8438df155195af80714dc12b321278ba0cbc515
[David <[nmueller855@gmail.com]>
2023-04-28 10:07:20 -0700

    406

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,8 +1,23 @@
-import { useFetchPhotosQuery } from '../store';
+import { useFetchPhotosQuery, useAddPhotoMutation } from '../store';
+import Button from './Button';
 
 function PhotosList({ album }) {
   useFetchPhotosQuery(album);
-  return 'PhotosList';
+  const [addPhoto, AddPhotoResults] = useAddPhotoMutation();
+
+  const handleAddPhoto = () => {
+    addPhoto(album);
+  };
+  return (
+    <div className='m-2 flex flex-row items-center justify-between'>
+      <div className='text-lg font-bold'>
+        <h3>Photos in {album.title}</h3>
+        <Button loading={AddPhotoResults.isLoading} onClick={handleAddPhoto}>
+          + Add Photo
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default PhotosList;
